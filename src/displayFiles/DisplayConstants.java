a3b4ee04d9a1f70ef968361b2e7cb337c8fd7607
chwoates <chwoates@gmail.com>
2020-12-14 17:55:37 -0700

    add fill method

--- src/displayFiles/DisplayConstants.java
+++ src/displayFiles/DisplayConstants.java
@@ -4,4 +4,7 @@ public final class DisplayConstants {
 
     public final static int FRAMEWIDTH = 1750;
     public final static int FRAMEHEIGHT = 1000;
+    public final static int XOFFSET = 100;
+    public final static int YOFFSET = 790;
+    public final static int POINTSPACING = 2;
 }
--- src/displayFiles/DisplayPaths.java
+++ src/displayFiles/DisplayPaths.java
@@ -9,28 +9,36 @@ import java.util.ArrayList;
 import static displayFiles.DisplayConstants.*;
 
 public class DisplayPaths {
+
     private int xCoord = 20, yCoord = 50;
     private Path pathPoints = new Path();
+    private Path userPoints = new Path();
+    private Path filledPoints = new Path();
     private MakePaths makePaths = new MakePaths();
+    private boolean filled = false;
+    JFrame frame = new JFrame();
+    JButton button = new JButton("Eat me");
+    MyDrawPanel drawPanel = new MyDrawPanel();
 
     public DisplayPaths() {
     }
 
     public void runDisplay() {
 
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Eat me");
-        MyDrawPanel drawPanel = new MyDrawPanel();
         frame.getContentPane().add(drawPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(FRAMEWIDTH, FRAMEHEIGHT);
         frame.setVisible(true);
+
+        displayUserPoints();
+        displayFilledPoints();
+
         pathPoints = makePaths.getPath();
 
         for(Point pts : pathPoints.getPath()){
-            xCoord = (int) (pts.getX());
-            yCoord = (int) (pts.getY());
-            drawPanel.repaint();
+           // xCoord = (int) (pts.getX());
+           // yCoord = (int) (pts.getY());
+            //drawPanel.repaint();
             try{
                 Thread.sleep(100);
             } catch(Exception ex){}
@@ -41,9 +49,51 @@ public class DisplayPaths {
 
         public void paintComponent(Graphics g) {
             g.setColor(Color.black);
-            g.drawLine(98,820,1500,820);
+            g.drawLine(98,800,1600,800);
             g.setColor(Color.red);
-            g.fillOval(xCoord,  yCoord, 10, 10);
+            if(filled)  g.setColor(Color.blue);
+            g.fillOval(xCoord*25 + XOFFSET,  YOFFSET-yCoord*15, 15, 15);
         }
+    }
+
+    public void displayUserPoints(){
+
+        userPoints.addPathPoint(new Point(0,0));
+        userPoints.addPathPoint(new Point(50,40));
+        userPoints.addPathPoint(new Point(20,0));
+        userPoints.addPathPoint(new Point(30,40));
+
+        for(Point pts : userPoints.getPath()) {
+            xCoord = (int) (pts.getX());
+            yCoord = (int) (pts.getY());
+            drawPanel.repaint();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    public void displayFilledPoints() {
+
+        int distance = (int) (userPoints.getPath().get(2).getX() - userPoints.getPath().get(0).getX());
+        int numPoints = distance / POINTSPACING;
+        for (int i = 0; i < numPoints; ++i) {
+            filledPoints.addPathPoint(new Point(userPoints.getPath().get(0).getX() + i*POINTSPACING,
+                    userPoints.getPath().get(0).getY()));
+        }
+        filled = true;
+        int j = 0;
+        for (Point pts : filledPoints.getPath()) {
+            xCoord = (int) (pts.getX());
+            yCoord = (int) (pts.getY());
+            if (j > 0) drawPanel.repaint();
+            ++j;
+            try {
+                Thread.sleep(200);
+            } catch (Exception ex) {
+            }
+        }
+        filled = false;
     }
 }
