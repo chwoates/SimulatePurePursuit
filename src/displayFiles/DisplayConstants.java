aaa0e62eca844b780eecd55f9e039a3dba504466
chwoates <chwoates@gmail.com>
2020-12-15 16:22:44 -0700

    added smoothing constant

--- src/displayFiles/DisplayConstants.java
+++ src/displayFiles/DisplayConstants.java
@@ -7,4 +7,5 @@ public final class DisplayConstants {
     public final static int XOFFSET = 100;
     public final static int YOFFSET = 700;
     public final static int POINTSPACING = 2;
+    public final static double SMOOTHING = 1.0;
 }
